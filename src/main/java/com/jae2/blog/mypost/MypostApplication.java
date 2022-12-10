9a6d6f91860953fc5420478c46e521e116a45815
jaebyoung <tqkrwoqud830@naver.com>
2022-12-10 17:00:36 +0900

    Initial commit

---
+++ src/main/java/com/jae2/blog/mypost/MypostApplication.java
@@ -0,0 +1,13 @@
+package com.jae2.blog.mypost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jae2/blog/mypost/MypostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jae2.blog.mypost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
